ecb90d11872fdbb882e1a9db0a18bbc09f83cb6a
salmaaghisn <97302116+salmaaghisn@users.noreply.github.com>
2022-03-23 17:56:11 +0700

    taxiApp

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi1 = new Taxi();
+            Taxi taxi2 = new Taxi();
+            Taxi taxi3 = new Taxi();
+            Taxi taxi4 = new Taxi();
+
+            taxi1.DriverName = "|| Muhammad ";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 47;
+
+            taxi2.DriverName = "|| Haiban ";
+            taxi2.OnDuty = true;
+            taxi2.NumPassenger = 20;
+
+            taxi3.DriverName = "|| Budi ";
+            taxi3.OnDuty = false;
+            taxi3.NumPassenger = 15;
+
+            taxi4.DriverName = "|| Ariawan ";
+            taxi4.OnDuty = false;
+            taxi4.NumPassenger = 76;
+
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassenger();
+            taxi1.DropOffPassenger();
+
+            taxi2.TaxiInfo();
+            taxi2.PickUpPassenger();
+            taxi2.DropOffPassenger();
+
+            taxi3.TaxiInfo();
+            taxi3.PickUpPassenger();
+            taxi3.DropOffPassenger();
+
+            taxi4.TaxiInfo();
+            taxi4.PickUpPassenger();
+            taxi4.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+        public string status { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver name: {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                status = "Yes";
+            }
+            else
+            {
+                status = "No"; 
+            }
+            Console.WriteLine("On Duty : {0}", status);
+            Console.WriteLine("Num Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("{0}  sedang menjemput penumpang ||", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang ||", DriverName);
+            Console.WriteLine("===========================================\n\n");
+        }
+    }
+}
